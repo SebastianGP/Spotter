26166a527f85b546200f3c3205906eec279d3e5b
Sebastian Puczynski <sebastian_puczynski@Sebastian-Puczynskis-MacBook-Pro.local>
2020-07-26 22:31:14 -0400

    Fixed redirect uri

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var port_number = process.env.PORT || 3000;
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = `http://localhost:8888/callback`; // Your redirect uri
+var redirect_uri = `https://spotter-utilities.herokuapp.com/callback/`; // Your redirect uri
 
 
 
